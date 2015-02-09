b326f4e5a29094991ed371c1676b743893705b30
jillskoba <redterror_117@hotmail.com>
2015-02-09 02:11:18 -0500

    Pickup Animation & Flashlight
    - Pickups now have animations (GameObject with mesh must be rotated 30
    degrees onto its side)
    - Converted mesh-based collision system to Raycast system (some bugs are
    persistent, including a 20% chance of falling through the floor after
    turning on the flashlight at the start, and spasming raycasts)

--- Unity Test Level/Up-To-Date/Assets/Scripts/Pickup.js
+++ Unity Test Level/Up-To-Date/Assets/Scripts/Pickup.js
@@ -1,9 +1,14 @@
 ﻿#pragma strict
 
+private var moveY : float;
+public var speed : float = 2;
+public var amplitude : float = 0.1;
+public var rotationSpeed : float = 1;
 function Start () {
-
+	moveY = transform.position.y;
 }
-
+         
 function Update () {
-
+	transform.position.y = moveY+amplitude*Mathf.Sin(speed*Time.time);
+	transform.Rotate(.5,rotationSpeed,0);
 }
