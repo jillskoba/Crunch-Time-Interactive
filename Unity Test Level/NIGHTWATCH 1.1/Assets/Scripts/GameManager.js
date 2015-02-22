1bcbef9683222cce33165a66b2479ac87ba33992
jillskoba <redterror_117@hotmail.com>
2015-02-22 18:56:11 -0500

    Version 1.1
    Code-collaborated version from 2/20/15

---
+++ Unity Test Level/NIGHTWATCH 1.1/Assets/Scripts/GameManager.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+var currentLevel : float = 0;
+var fragments : float = 0;
+var batteries : float = 0;
+var batteryLimit : float = 5;
+var character : float = 1;
+
+function Start() {
+
+}
+
+function PickupFragment () {
+	fragments += 1;
+}
+
+function PickupBattery () {
+	batteries += 1;
+}
+
+function UseBattery () {
+	batteries -= 1;
+}
+
+function Awake () {
+	DontDestroyOnLoad (gameObject);
+}
+
+function LoadNextLevel () {
+	currentLevel += 1;
+	Application.LoadLevel(currentLevel);
+}
