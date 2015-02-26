3fb86f7a190177b640884278b7499bfd83303aea
jillskoba <redterror_117@hotmail.com>
2015-02-26 03:57:45 -0500

    Version 1.2
    Raycasting for light collision should be fully functional
    - length of ray is reduced when colliding with objects
    - rays properly turn with character
    - rays are not aligned to the character's light source, but down the
    middle of the character instead, to prevent issues with enemy collisions

---
+++ Unity Test Level/NIGHTWATCH 1.2/Assets/Scripts/playerCollisions.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+public var gameManager : GameObject;
+
+function Start () {
+	gameManager = GameObject.Find("gameManager");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(hit : Collider) {
+	if (hit.tag == "battery" && gameManager.GetComponent(GameManager).batteries < gameManager.GetComponent(GameManager).batteryLimit) {
+		gameManager.GetComponent(GameManager).PickupBattery();
+		Destroy(hit.gameObject);
+	}
+	if (hit.tag == "fragment") {
+		gameManager.GetComponent(GameManager).PickupFragment();
+		Destroy(hit.gameObject);
+	}
+	
+	if (hit.tag == "levelend") {
+		gameManager.GetComponent(GameManager).LoadNextLevel();
+	}
+
+	if (hit.tag=='weakSpot'){
+		//rigidbody.velocity.y = jumpHeight;
+		Destroy(hit.gameObject.transform.parent.gameObject);
+		Debug.Log("EnemyHit");
+	}
+
+	if(hit.tag == "wall"){
+		collider.material.staticFriction = 0;
+		collider.material.dynamicFriction = 0;
+	}
+	//handle moving platform collision
+	if(hit.tag == "movingplatform"){
+		collider.material.staticFriction = 1;
+		collider.material.dynamicFriction = 1;
+	}
+}
