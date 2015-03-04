5adde705b41988eb1492bb4513609e99430b83f4
jillskoba <redterror_117@hotmail.com>
2015-03-04 16:17:11 -0500

    1.3
    Uploading 1.3 build

---
+++ Unity Test Level/NIGHTWATCH 1.3/Assets/Scripts/FlashlightLight.js
@@ -0,0 +1,200 @@
+﻿#pragma strict
+
+public var player: GameObject;
+
+var distanceTop : float = 6.65;
+var distanceTopMid : float = 6.5;
+var distanceMid : float = 6.35;
+var distanceBottomMid : float = 6.2;
+var distanceBottom : float = 6.05;
+
+var distanceTopBase : float = 6.65;
+var distanceTopMidBase : float = 6.5;
+var distanceMidBase : float = 6.35;
+var distanceBottomMidBase : float = 6.2;
+var distanceBottomBase : float = 6.05;
+
+var angleTop : float = 0.04;
+var angleTopMid : float = 0.14;
+var angleMid : float = 0.24;
+var angleBottomMid : float = 0.34;
+var angleBottom : float = 0.44;
+
+var center : Vector3;
+var originOffsetX : float = 0;
+var originOffsetY : float = -0.05;
+var originOffsetZ : float = 0;
+var beamZOffset : float = 0;
+
+var hitTop : RaycastHit;
+var hitTopMid : RaycastHit;
+var hitMid : RaycastHit;
+var hitBottomMid : RaycastHit;
+var hitBottom : RaycastHit;
+
+private var characterRight : boolean;
+
+
+function Start () {
+	characterRight = true;
+}
+
+function Update () {
+	if (rigidbody.velocity.x < 0) {
+		characterRight = false;
+	} else if (rigidbody.velocity.x > 0){
+		characterRight = true;
+	}
+	if (player.GetComponent(Flashlight).lightOn) {
+		flashLightHit();
+	}
+	
+}
+
+function flashLightHit() {
+	if (characterRight) {
+		center = transform.position;
+		center.x += originOffsetX;
+		center.y += originOffsetY;
+		center.z += originOffsetZ;
+		
+		//Top
+		if (Physics.Raycast (center, Vector3(distanceTop+0.5,-distanceTop*angleTop,beamZOffset), hitTop)) {
+			if (hitTop.distance > distanceTopBase) {
+				distanceTop = distanceTopBase;
+			} else {
+				distanceTop = hitTop.distance;
+			}
+			if(hitTop.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Top-Mid
+		if (Physics.Raycast (center, Vector3(distanceTopMid+0.5,-distanceTopMid*angleTopMid,beamZOffset), hitTopMid)) {
+			if (hitTopMid.distance > distanceTopMidBase) {
+				distanceTopMid = distanceTopMidBase;
+			} else {
+				distanceTopMid = hitTopMid.distance;
+			}
+			if(hitTopMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Mid
+		if (Physics.Raycast (center, Vector3(distanceMid+0.25,-distanceMid*angleMid,beamZOffset), hitMid)) {
+			if (hitMid.distance > distanceMidBase) {
+				distanceMid = distanceMidBase;
+			} else {
+				distanceMid = hitMid.distance;
+			}
+			if(hitMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Bottom-Mid
+		if (Physics.Raycast (center, Vector3(distanceBottomMid,-distanceBottomMid*angleBottomMid,beamZOffset), hitBottomMid)) {
+			if (hitBottomMid.distance > distanceBottomMidBase) {
+				distanceBottomMid = distanceBottomMidBase;
+			} else {
+				distanceBottomMid = hitBottomMid.distance;
+			}
+			if(hitBottomMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Bottom
+		if (Physics.Raycast (center, Vector3(distanceBottom,-distanceBottom*angleBottom,beamZOffset), hitBottom)) {
+			if (hitBottom.distance > distanceBottomBase) {
+				distanceBottom = distanceBottomBase;
+			} else {
+				distanceBottom = hitBottom.distance;
+			}
+			if(hitBottom.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		Debug.DrawRay (center, Vector3(distanceTop,-distanceTop*angleTop,beamZOffset), Color.red); //Top
+		Debug.DrawRay (center, Vector3(distanceTopMid,-distanceTopMid*angleTopMid,beamZOffset), Color.red); //Top-Mid
+		Debug.DrawRay (center, Vector3(distanceMid,-distanceMid*angleMid,beamZOffset), Color.red); //Mid
+		Debug.DrawRay (center, Vector3(distanceBottomMid,-distanceBottomMid*angleBottomMid,beamZOffset), Color.red); //Bottom-Mid
+		Debug.DrawRay (center, Vector3(distanceBottom,-distanceBottom*angleBottom,beamZOffset), Color.red); //Bottom
+		
+	} else {
+		center = transform.position;
+		center.x -= originOffsetX;
+		center.y -= originOffsetY;
+		center.z -= originOffsetZ;
+		
+		//Top
+		if (Physics.Raycast (center, Vector3(-distanceTop+0.5,-distanceTop*angleTop,-beamZOffset), hitTop)) {
+			if (hitTop.distance > distanceTopBase) {
+				distanceTop = distanceTopBase;
+			} else {
+				distanceTop = hitTop.distance;
+			}
+			if(hitTop.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Top-Mid
+		if (Physics.Raycast (center, Vector3(-distanceTopMid+0.5,-distanceTopMid*angleTopMid,-beamZOffset), hitTopMid)) {
+			if (hitTopMid.distance > distanceTopMidBase) {
+				distanceTopMid = distanceTopMidBase;
+			} else {
+				distanceTopMid = hitTopMid.distance;
+			}
+			if(hitTopMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Mid
+		if (Physics.Raycast (center, Vector3(-distanceMid+0.25,-distanceMid*angleMid,-beamZOffset), hitMid)) {
+			if (hitMid.distance > distanceMidBase) {
+				distanceMid = distanceMidBase;
+			} else {
+				distanceMid = hitMid.distance;
+			}
+			if(hitMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Bottom-Mid
+		if (Physics.Raycast (center, Vector3(-distanceBottomMid,-distanceBottomMid*angleBottomMid,-beamZOffset), hitBottomMid)) {
+			if (hitBottomMid.distance > distanceBottomMidBase) {
+				distanceBottomMid = distanceBottomMidBase;
+			} else {
+				distanceBottomMid = hitBottomMid.distance;
+			}
+			if(hitBottomMid.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		//Bottom
+		if (Physics.Raycast (center, Vector3(-distanceBottom,-distanceBottom*angleBottom,-beamZOffset), hitBottom)) {
+			if (hitBottom.distance > distanceBottomBase) {
+				distanceBottom = distanceBottomBase;
+			} else {
+				distanceBottom = hitBottom.distance;
+			}
+			if(hitBottom.collider.tag == 'enemy'){
+				Debug.Log('IS A BADDIE!');
+			}
+		}
+		
+		Debug.DrawRay (center, Vector3(-distanceTop,-distanceTop*angleTop,-beamZOffset), Color.red); //Top
+		Debug.DrawRay (center, Vector3(-distanceTopMid,-distanceTopMid*angleTopMid,-beamZOffset), Color.red); //Top-Mid
+		Debug.DrawRay (center, Vector3(-distanceMid,-distanceMid*angleMid,-beamZOffset), Color.red); //Mid
+		Debug.DrawRay (center, Vector3(-distanceBottomMid,-distanceBottomMid*angleBottomMid,-beamZOffset), Color.red); //Bottom-Mid
+		Debug.DrawRay (center, Vector3(-distanceBottom,-distanceBottom*angleBottom,-beamZOffset), Color.red); //Bottom
+		
+	}
+}
---
+++ Unity Test Level/NIGHTWATCH 1.3/Assets/Scripts/playerCollisions.js
@@ -0,0 +1,59 @@
+﻿#pragma strict
+
+private var gameManager : GameObject;
+
+function Start () {
+	gameManager = GameObject.Find("gameManager");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(hit : Collider) {
+	//----------PICKUPS----------
+	if (hit.tag == "battery" && gameManager.GetComponent(GameManager).batteries < gameManager.GetComponent(GameManager).batteryLimit) {
+		gameManager.GetComponent(GameManager).PickupBattery();
+		Destroy(hit.gameObject);
+	}
+
+	if (hit.tag == "fragment") {
+		gameManager.GetComponent(GameManager).PickupFragment();
+		Destroy(hit.gameObject);
+	}
+
+	if (hit.tag == "overcharge" && gameManager.GetComponent(GameManager).overcharge < gameManager.GetComponent(GameManager).overchargeLimit) {
+	//if (hit.tag == "overcharge"){
+		gameManager.GetComponent(GameManager).PickupOvercharge();
+		Destroy(hit.gameObject);
+	}
+	
+	
+	//----------UTILITY----------
+	if (hit.tag == "levelend") {
+		gameManager.GetComponent(GameManager).LoadNextLevel();
+	}
+	
+	if(hit.tag == "wall"){
+		collider.material.staticFriction = 0;
+		collider.material.dynamicFriction = 0;
+	}
+	//handle moving platform collision
+	if(hit.tag == "movingplatform"){
+		collider.material.staticFriction = 1;
+		collider.material.dynamicFriction = 1;
+	}
+	
+	
+	//----------ENEMY----------
+	if (hit.tag=='weakSpot'){
+		//rigidbody.velocity.y = jumpHeight;
+		Destroy(hit.gameObject.transform.parent.gameObject);
+		//Debug.Log("EnemyHit");
+	}
+	
+	if (hit.tag == "enemy") {
+		gameManager.GetComponent(GameManager).LoseHealth();
+		Debug.Log("Hit by an enemy! Health remaining: "+gameManager.GetComponent(GameManager).health);
+	}
+}
