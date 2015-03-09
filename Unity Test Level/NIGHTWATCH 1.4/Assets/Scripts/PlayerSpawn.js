484c0098cb2ad493418e057dc827e42b90b35fbc
jillskoba <redterror_117@hotmail.com>
2015-03-09 00:06:54 -0400

    1.4
    GUI is enabled:
    - Health and Flashlight Power are now visible
    - Fragments collected are shown
    - Inventory is visible
    
    Flashlight functionality scripting (Flashlight.js) has been merged into
    GameManager.js and PlayerController.js, in order to allow the Game
    Manager access to the flashlight power variables. The script has been
    left in, but disabled on the player proxies.

---
+++ Unity Test Level/NIGHTWATCH 1.4/Assets/Scripts/PlayerSpawn.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function Start () {
+	var manager: GameManager = GameObject.Find("gameManager").GetComponent("GameManager");
+	Debug.Log(manager.character);
+	var character : GameObject = Instantiate(Resources.Load("P"+ manager.character), gameObject.transform.position, gameObject.transform.rotation);
+	//var character : GameObject = Instantiate(Resources.Load("Player"), gameObject.transform.position, gameObject.transform.rotation);
+}
