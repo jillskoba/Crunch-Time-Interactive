6435306c9beba6b40acf182bc736f686b8e5a4c1
jillskoba <redterror_117@hotmail.com>
2015-03-09 18:36:09 -0400

    1.4.1, Old version archived
    Versions 1.0-1.2 have been removed and archived to free more space on
    GitHub; version 1.4.1 implements Jake's level. Book object removed to
    free space (I had not realized at the time the book's texture was 9MB)

--- Unity Test Level/NIGHTWATCH 1.2/Assets/Scripts/PlayerSpawn.js
+++
@@ -1,8 +0,0 @@
-﻿#pragma strict
-
-function Start () {
-	var manager: GameManager = GameObject.Find("gameManager").GetComponent("GameManager");
-	Debug.Log(manager.character);
-	var character : GameObject = Instantiate(Resources.Load("P"+ manager.character), gameObject.transform.position, gameObject.transform.rotation);
-	//var character : GameObject = Instantiate(Resources.Load("Player"), gameObject.transform.position, gameObject.transform.rotation);
-}
