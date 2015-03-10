1911dbef9ba3f2b22e07c8a8109cdc865bf8a167
jillskoba <redterror_117@hotmail.com>
2015-03-10 01:55:05 -0400

    1.5
    Updated GUI, Enemies, Overcharge. Added Levels

---
+++ Unity Test Level/NIGHTWATCH 1.5/Assets/Scripts/GameManager.js
@@ -0,0 +1,260 @@
+﻿#pragma strict
+var currentLevel : int = 0;
+var playerHealth : int = 3; //Character Health
+var character : float = 1; //Character Selection
+
+//-------CHARACTER INVENTORY---------
+var fragments : int = 0;
+var batteries : int = 0;
+var overcharge : int = 0;
+var overchargeLimit : int = 1;
+var batteryLimit : int = 5;
+
+
+//-------GAME GUI---------
+public var fragmentIcon : Texture2D;
+public var batteryItemIcon : Texture2D;
+public var overchargeItemIcon : Texture2D;
+
+public var statusPlateBack : Texture2D;
+public var statusPlateFront : Texture2D;
+
+public var healthBar : Texture2D;
+public var healthBarEnd : Texture2D;
+
+public var powerBar : Texture2D;
+public var powerBarEnd : Texture2D;
+
+public var inventoryPlate : Texture2D;
+public var highlight: Texture2D;
+
+//fragment icon dimensions
+public var fragmentIconWidth : float;
+public var fragmentIconHeight : float;
+
+//battery icon dimensions
+public var batteryIconWidth : float;
+public var batteryIconHeight : float;
+
+//overcharge icon dimensions
+public var overchargeIconWidth : float;
+public var overchargeIconHeight : float;
+
+//inventory plate dimensions
+public var inventoryPlateWidth : float;
+public var inventoryPlateHeight : float;
+
+//health bar dimensions
+public var healthWidth : float;
+public var healthHeight : float;
+
+//power bar dimensions
+public var powerWidth : float;
+public var powerHeight : float;
+public var pbEndWidth : float;
+public var pbEndHeight : float;
+
+//health frame dimensions
+public var statusFrameWidth : float;
+public var statusFrameHeight : float;
+
+//health frame padding
+public var statusTopMarginLeft : float;
+public var statusTopMarginTop : float;
+
+//Health Bar additional variables
+private var maxPlayerHealth : float; //capture the total possible amount of health a player can have in-game
+public var healthChunk : float; //captures 'chunks' of the health bar, according to Health Bar Width / Player Health
+
+//Power Bar additional variables
+private var maxPlayerPower : float; //capture the total possible amount of power a player can have in-game
+public var powerChunk : float; //captures 'chunks' of the power bar, according to Power Bar Width / Player Power
+
+//-------Flashlight Copy-over-------//
+public var flashlightPower : float = 100;
+public var flashlightDecaySpeed : float = 25;
+public var flashlightRecoverPower : float = 20;
+public var lightOn : boolean = false;
+
+public var overchargeLength : float = 10;
+public var overchargeActive : boolean = false;
+
+public var powerupSelect : int = 1;
+
+
+//----------------------------------------------END DECLARATIONS------------------------------------
+
+function Start() {
+	maxPlayerHealth = playerHealth;
+	healthChunk = healthWidth / maxPlayerHealth * playerHealth;
+	
+	maxPlayerPower = flashlightPower;
+	powerChunk = powerWidth / maxPlayerPower * flashlightPower;
+	
+	InvokeRepeating("FlashlightPower", 1, .5); 
+	lightOn = false;
+	overchargeActive = false;
+	
+}
+
+function Update() {
+	healthChunk = healthWidth / maxPlayerHealth * playerHealth;
+	powerChunk = powerWidth / maxPlayerPower * flashlightPower;
+	
+	//Activate Light
+	if (Input.GetButtonDown ("Fire1") && flashlightPower > 0){ 
+		lightOn = !lightOn;
+	}
+	
+	if(Input.GetAxis("Vertical") > 0) {
+		powerupSelect = 1;
+	} else if (Input.GetAxis("Vertical") < 0) {
+		powerupSelect = 2;
+	}
+	
+	//Keeps power from exceeding 100
+	if (flashlightPower > 100) {
+		flashlightPower = 100;
+	}
+	
+	// Kills flashlight when it runs out of power
+    if (flashlightPower <= 0) {
+    	lightOn = false;
+    }
+	
+}
+
+
+//----------HEALTH----------
+function GainHealth () {
+	if (playerHealth < 3) {
+		playerHealth += 1;
+	} else {
+		playerHealth = 3;
+	}
+}
+
+function LoseHealth () {
+	if (playerHealth > 0) {
+		playerHealth -= 1;
+	} else {
+		playerHealth = 0;
+	}
+}
+
+//----------FRAGMENTS---------
+function PickupFragment () {
+	fragments += 1;
+}
+
+//----------BATTERIES----------
+function PickupBattery () {
+	if (batteries == 0) {
+		batteryItemIcon = Resources.Load("batteryIcon-Active") as Texture2D;
+	}
+	batteries += 1;
+}
+
+function UseBattery () {
+	batteries -= 1;
+	if (batteries == 0) {
+		batteryItemIcon = Resources.Load("batteryIcon-Inactive") as Texture2D;
+	}
+	flashlightPower += flashlightRecoverPower;
+}
+
+//----------OVERCHARGE----------
+function PickupOvercharge () {
+	if (overcharge == 0) {
+			overchargeItemIcon = Resources.Load("overchargeIcon-Active") as Texture2D;
+		}
+	overcharge += 1;
+}
+
+function UseOvercharge () {
+	if (overcharge > 0){
+		overcharge -= 1;
+	} else {
+		overcharge = 0;
+	}
+	if (overcharge == 0) {
+		overchargeItemIcon = Resources.Load("overchargeIcon-Inactive") as Texture2D;
+	}
+	overchargeActive = true;
+	audio.Play();
+	yield WaitForSeconds(overchargeLength);
+    overchargeCooldown();
+}
+
+function overchargeCooldown() {
+	overchargeActive = false;
+	audio.Pause();
+}
+
+//----------LEVEL----------
+
+function Awake () {
+	DontDestroyOnLoad (gameObject);
+}
+
+function LoadNextLevel () {
+	currentLevel += 1;
+	Application.LoadLevel(currentLevel);
+}
+
+function LoadLevel(){
+	Application.LoadLevel(currentLevel);
+}
+
+//----------Flashlight-------
+
+function FlashlightPower (){
+	if (lightOn) {
+		flashlightPower -= flashlightDecaySpeed * Time.deltaTime;
+	}
+}
+
+//----------GUI----------
+
+function OnGUI() {
+	//var customSkin : GUISkin = Resources.Load("myGUI") as GUISkin;
+
+	var whiteText = GUI.skin.GetStyle("Label");
+	whiteText.richText = true;
+	whiteText.fontSize = 16;
+
+	//GUI.skin = customSkin;
+	GUI.contentColor = Color.black;
+	
+	if (Application.loadedLevel >= 2) { //if we're in an actual game level
+	
+		//Health Bar
+		GUI.DrawTexture(Rect(statusTopMarginLeft, statusTopMarginTop,statusFrameWidth,statusFrameHeight), statusPlateBack); //status plate back
+		GUI.DrawTexture(Rect(statusTopMarginLeft + 34, statusTopMarginTop,healthChunk,healthHeight), healthBar); //health bar
+		GUI.DrawTexture(Rect(statusTopMarginLeft + 34 + healthChunk, statusTopMarginTop,13,28), healthBarEnd); //health bar end
+		
+		GUI.DrawTexture(Rect(statusTopMarginLeft + 34, statusTopMarginTop + 27,powerChunk,powerHeight), powerBar); //power bar
+		//GUI.DrawTexture(Rect(statusTopMarginLeft + 34 + powerChunk, statusTopMarginTop + 27,pbEndWidth,pbEndHeight), powerBarEnd); //power bar end
+		
+		GUI.DrawTexture(Rect(statusTopMarginLeft, statusTopMarginTop,statusFrameWidth,statusFrameHeight), statusPlateFront); //status plate front
+		
+		//Fragment Count
+		GUI.DrawTexture(new Rect(Screen.width - Screen.width, Screen.height - 50,fragmentIconWidth,fragmentIconHeight), fragmentIcon); //fragment icon
+		GUI.Label(Rect(Screen.width - Screen.width + 36, Screen.height - 40,75,75), "<color=white>" + fragments + "</color>", whiteText); //fragment count
+		 
+		 //Inventory
+		 GUI.DrawTexture(new Rect(Screen.width - 68, 10, inventoryPlateWidth, inventoryPlateHeight), inventoryPlate); //inventory plate
+		 GUI.DrawTexture(new Rect(Screen.width - 49, 30, batteryIconWidth, batteryIconHeight), batteryItemIcon); //battery inventory icon
+		 GUI.DrawTexture(new Rect(Screen.width - 52, 95, overchargeIconWidth, overchargeIconHeight), overchargeItemIcon); //overcharge inventory icon
+		 GUI.Label(Rect(Screen.width - 27, 52,75,75), "<color=white>" + batteries + "</color>", whiteText); //battery count
+		 GUI.Label(Rect(Screen.width - 27, 115,75,75), "<color=white>" + overcharge + "</color>", whiteText); //overcharge count
+		 
+		 if (powerupSelect == 1) {
+		 	GUI.DrawTexture(Rect(Screen.width - 63, 25, 50, 50), highlight); //batteryHover
+		 }
+		 
+		 if (powerupSelect == 2) {
+		 	GUI.DrawTexture(Rect(Screen.width - 63, 88, 50, 50), highlight); //overchargeHover
+		 }
+	}
+}
---
+++ Unity Test Level/NIGHTWATCH 1.5/Assets/Scripts/playerCollisions.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+public var gameManager : GameObject;
+
+
+function Start () {
+	gameManager = GameObject.Find("gameManager");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(hit : Collider) {
+	if (hit.tag == "battery" && gameManager.GetComponent(GameManager).batteries < gameManager.GetComponent(GameManager).batteryLimit) {
+		gameManager.GetComponent(GameManager).PickupBattery();
+		Destroy(hit.gameObject);
+	}
+	
+	if (hit.tag == "overcharge" && gameManager.GetComponent(GameManager).overcharge < gameManager.GetComponent(GameManager).overchargeLimit) {
+		gameManager.GetComponent(GameManager).PickupOvercharge();
+		Destroy(hit.gameObject);
+	}
+	
+	if (hit.tag == "fragment") {
+		gameManager.GetComponent(GameManager).PickupFragment();
+		Destroy(hit.gameObject);
+	}
+	
+	if (hit.tag == "levelend") {
+		gameManager.GetComponent(GameManager).LoadNextLevel();
+	}
+
+	if (hit.tag=='weakSpot'){
+		//rigidbody.velocity.y = jumpHeight;
+		Destroy(hit.gameObject.transform.parent.gameObject);
+		Debug.Log("EnemyHit");
+	}
+
+	if(hit.tag == "wall"){
+		collider.material.staticFriction = 0;
+		collider.material.dynamicFriction = 0;
+	}
+	//handle moving platform collision
+	if(hit.tag == "movingplatform"){
+		collider.material.staticFriction = 1;
+		collider.material.dynamicFriction = 1;
+	}
+	//Added Enemy Collisions, example enemy model hitboxes need light tweaking will fix later this week
+	if(hit.tag == "enemy"){
+		if (gameManager.GetComponent(GameManager).overchargeActive == true) {
+			Destroy(hit.gameObject);
+		} else {
+			var enemy: enemyAI = hit.gameObject.GetComponent('enemyAI');
+			//If enemy is undefined, find the enemy tag's parent
+			if(enemy == null){
+				enemy = hit.gameObject.transform.parent.gameObject.GetComponent('enemyAI');
+			}
+			Debug.Log('Player collided with enemy type ' + enemy.enemyType);
+			gameManager.GetComponent(GameManager).LoseHealth();
+		}
+	}
+	
+	//For projectile shooter
+	if(hit.tag == "projectile"){
+		Debug.Log('Hit By a projectile!');
+	}
+	
+}
