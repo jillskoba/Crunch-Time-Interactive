1813146c54a2bfa51ef132f07860faf559d1959b
DRieder <dylan@d-rieder.com>
2015-03-19 17:04:27 -0400

    Reworked Guard Enemy Patrol function
    Using David's moving platform script I tweaked the enemy pathing to work
    correctly.

---
+++ Unity Test Level/NIGHTWATCH 1.7.2/Assets/Scripts/Flashlight.js
@@ -0,0 +1,63 @@
+﻿#pragma strict
+
+public var flashlight : Light;
+public var beam: GameObject;
+public var flickerSpeed : float = 0.7;
+public var flashlightPower : float = 100;
+public var flashlightDecaySpeed : float = 0.1;
+public var flashlightRecoverPower : int = 20;
+public var lightOn : boolean = false;
+private var gameManager : GameObject;
+
+function Start () {
+	InvokeRepeating("FlashlightPower", 1, .5); 
+	lightOn = false;
+	flashlight.enabled = lightOn;
+	beam.renderer.enabled = false;
+	gameManager = GameObject.Find("gameManager");
+}
+
+function Update () {
+	if (Input.GetButtonDown ("Fire1") && flashlightPower > 0){ 
+		lightOn = !lightOn;
+		flashlight.enabled = lightOn;
+		Debug.Log(flashlightPower);
+	}
+	
+	if (Input.GetButtonDown ("Fire2") && flashlightPower < 100 && gameManager.GetComponent(GameManager).batteries > 0){ 
+		flashlightPower += flashlightRecoverPower;
+		gameManager.GetComponent(GameManager).UseBattery();
+	}
+	 
+	if (flashlightPower > 100) {
+		flashlightPower = 100;
+	}
+	
+	if (lightOn) {
+		if (flashlightPower <= 20) {
+	        if (Random.value > flickerSpeed ){
+	           if (flashlight.enabled == true ){
+	             flashlight.enabled = false;
+	             beam.renderer.enabled = false;
+	           }
+	           else{
+	             flashlight.enabled = true;
+	             beam.renderer.enabled = true;
+	           }
+	        }
+        }
+        
+        // Kills flashlight when it runs out of power
+        if (flashlightPower <= 0) {
+        	lightOn = false;
+        	flashlight.enabled = lightOn;
+        	beam.renderer.enabled = false;
+        }
+	}
+}
+
+function FlashlightPower (){
+	if (lightOn) {
+		flashlightPower -= 1 * Time .deltaTime;
+	}
+}
