cc3bb6dc2e5d36eb730ac2ddf105ff0a60a33b6b
DRieder <dylan@d-rieder.com>
2015-03-30 11:54:37 -0700

    Created Logo and Designed About page
    Site seems to work in IE enviroment so long as activex is enabled

--- nightwatchWebsite/js/main.js
+++ nightwatchWebsite/js/main.js
@@ -6,10 +6,15 @@ function init() {
     	var main = document.querySelector("#mainContent");
     	var content = document.getElementsByClassName("content");
 
+
     	TweenLite.to(cover, 1, {display: 'none', opacity: 0});
     	TweenLite.to(main, 1, {display: 'block', opacity: 1, delay:1});
 		TweenLite.to(content, 1, {display: 'block', opacity: 1, delay:2});
 	});
 }
-
+function swapActive(){
+	var nav = document.querySelectorAll("#myTab li");
+	nav[0].className="";
+	nav[1].className="active";
+}
 window.addEventListener("load", init, false);
